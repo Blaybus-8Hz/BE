8bdbe972354760deb5553fcbfa067bac745d30c5
혜원 <bogum1029@gmail.com>
2025-02-15 23:40:09 +0900

    fix: JwtProperties 개별 필드 값 주입으로 변경

--- src/main/java/com/haertz/be/common/jwt/properties/JwtProperties.java
+++ src/main/java/com/haertz/be/common/jwt/properties/JwtProperties.java
@@ -2,13 +2,18 @@ package com.haertz.be.common.jwt.properties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
-@AllArgsConstructor
-@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+
+    @Value("${jwt.secret-key}")
     private String secretKey;
+
+    @Value("${jwt.access-token-exp}")
     private Long accessTokenExp;
+
+    @Value("${jwt.refresh-token-exp}")
     private Long refreshTokenExp;
 }
