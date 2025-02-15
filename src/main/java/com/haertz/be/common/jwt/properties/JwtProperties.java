e183f3b5230fe0590eb54226ab1c2dedd8773fc2
혜원 <bogum1029@gmail.com>
2025-02-16 00:10:52 +0900

    fix: 환경 변수 주입 방법 변경

--- src/main/java/com/haertz/be/common/jwt/properties/JwtProperties.java
+++ src/main/java/com/haertz/be/common/jwt/properties/JwtProperties.java
@@ -1,20 +1,14 @@
 package com.haertz.be.common.jwt.properties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-
-@Component
 @Getter
+@AllArgsConstructor
+@ConfigurationProperties("jwt")
 public class JwtProperties {
-
-    @Value("${jwt.secret-key}")
     private String secretKey;
-
-    @Value("${jwt.access-token-exp}")
     private Long accessTokenExp;
-
-    @Value("${jwt.refresh-token-exp}")
     private Long refreshTokenExp;
 }
