3ac045064c9ef9ef6785a53e101f0be94eb74cae
byunhm02 <128574532+byunhm02@users.noreply.github.com>
2025-02-16 12:03:28 +0900

    refactor: 의존성 주입 방식을 생성자 주입으로 변경(#14)

--- src/main/java/com/haertz/be/Payment/controller/BankTransferController.java
+++ src/main/java/com/haertz/be/Payment/controller/BankTransferController.java
@@ -5,6 +5,7 @@ import com.haertz.be.payment.dto.BankTransferDto;
 import com.haertz.be.payment.dto.BankTransferRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "계좌이체 API", description = "계좌이체를 위한 API입니다.")
 @RestController
+@RequiredArgsConstructor
 @Log
 public class BankTransferController {
+    private final BankTransferService bankTransferService;
     @Operation(summary = "계좌이체 요청을 보냅니다.")
     @PostMapping("/api/banktransfer")
     public ResponseEntity<BankTransferDto> bankTransfer(@RequestBody BankTransferRequestDto requestDTO) {
-
-        BankTransferDto bankTransferDto = BankTransferService.banktransferrequest(requestDTO);
+        BankTransferDto bankTransferDto = bankTransferService.banktransferrequest(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(bankTransferDto);
     }
 }
