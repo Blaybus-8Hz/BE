9900da20cc97a217f18e1330b7f8fcde38cfb180
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-17 20:01:17 +0900

    feat : 과거/현재 예약리스트 조회 api (#6)

--- src/main/java/com/haertz/be/booking/service/BookingService.java
+++ src/main/java/com/haertz/be/booking/service/BookingService.java
@@ -2,13 +2,10 @@ package com.haertz.be.booking.service;
 
 import com.haertz.be.auth.adaptor.UserAdaptor;
 import com.haertz.be.auth.entity.User;
-import com.haertz.be.auth.exception.UserErrorCode;
 import com.haertz.be.booking.entity.Booking;
 import com.haertz.be.booking.repository.BookingRepository;
-import com.haertz.be.common.exception.base.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +15,7 @@ import java.util.stream.Collectors;
 public class BookingService {
     private final BookingRepository bookingRepository;
     private final UserAdaptor userAdaptor;
+
     public List<Booking> getPastBookings(Long userId){
         User user = userAdaptor.findById(userId);
         LocalDateTime now = LocalDateTime.now();
