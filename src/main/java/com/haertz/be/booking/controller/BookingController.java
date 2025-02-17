acf831c03cf304b8840771114744ec500fcd9111
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-17 20:01:17 +0900

    feat : 과거/현재 예약리스트 조회 api (#6)

--- src/main/java/com/haertz/be/booking/controller/BookingController.java
+++ src/main/java/com/haertz/be/booking/controller/BookingController.java
@@ -1,29 +1,35 @@
 package com.haertz.be.booking.controller;
 
+import com.haertz.be.booking.entity.Booking;
+import com.haertz.be.booking.service.BookingService;
 import com.haertz.be.common.response.SuccessResponse;
-import com.sun.net.httpserver.Authenticator;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //@Tag("")
 @RestController
+@RequiredArgsConstructor
 class BookingController {
 
+    private final BookingService bookingService;
     @Operation(summary = "사용자의 다가오는 예약 내역을 조회합니다.")
-    @GetMapping("/api/current")
-    public SuccessResponse<Object> getBookings(){
+    @GetMapping("/api/current/{userId}")
+    public SuccessResponse<Object> getBookings(@PathVariable("userId") Long userId){
+        List<Booking> bookingList = bookingService.getCurrentBookings(userId);
 
-
-        return SuccessResponse.of(null); //일단 이거를 null로 넣어두고
+        return SuccessResponse.of(bookingList);
     }
 
     @Operation(summary = "사용자의 지난 예약 내역을 조회합니다.")
-    @GetMapping("/api/past")
-    public SuccessResponse<Object> getPastBookings(){
+    @GetMapping("/api/past/{userId}")
+    public SuccessResponse<Object> getPastBookings(@PathVariable("userId") Long userId){
+        List<Booking> bookingList = bookingService.getPastBookings(userId);
 
-        return SuccessResponse.of(null);
+        return SuccessResponse.of(bookingList);
     }
 }
