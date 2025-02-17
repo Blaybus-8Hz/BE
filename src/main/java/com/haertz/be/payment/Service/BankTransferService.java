31a277a1e21a47d4ca1641216ab40713d394e10b
byunhm02 <128574532+byunhm02@users.noreply.github.com>
2025-02-17 22:28:56 +0900

    feat: 결제 후 구글미팅링크 생성 함수 호출 구현, 주석처리(#13)

--- src/main/java/com/haertz/be/payment/Service/BankTransferService.java
+++ src/main/java/com/haertz/be/payment/Service/BankTransferService.java
@@ -1,6 +1,9 @@
 package com.haertz.be.payment.Service;
 
 import com.haertz.be.common.exception.base.BaseException;
+import com.haertz.be.googlemeet.dto.GoogleMeetDto;
+import com.haertz.be.googlemeet.dto.GoogleMeetRequestDto;
+import com.haertz.be.googlemeet.service.GoogleMeetService;
 import com.haertz.be.payment.dto.BankTransferDto;
 import com.haertz.be.payment.dto.BankTransferRequestDto;
 import com.haertz.be.payment.dto.PaymentSaveDto;
@@ -18,6 +21,7 @@ import java.util.Date;
 @Log
 public class BankTransferService {
     private final PaymentSaveService paymentSaveService;
+    private final GoogleMeetService googleMeetService;
 
     public BankTransferDto banktransferrequest(BankTransferRequestDto requestDTO) {
         // 계좌이체 요청 정보가 유효한지 확인
@@ -33,10 +37,22 @@ public class BankTransferService {
             paymentSaveDto.setTotalAmount(new BigDecimal(requestDTO.getTotal_amount()));
             log.info(paymentSaveDto.toString());
             paymentSaveService.savePayment(paymentSaveDto);
+            /*
+            //구글 미팅 링크 생성
+            GoogleMeetRequestDto googleMeetRequestDto = new GoogleMeetRequestDto();
+            googleMeetRequestDto.setReservationId(1L); //현재는 테스트용. 예약엔티티 설정 후 변경.
+            googleMeetRequestDto.setUserId(Long.valueOf(requestDTO.getPartner_user_id()));
+            googleMeetRequestDto.setGooglemeetaccessToken("ya29.a0AXeO80RuWDPXpOuB--diMWqZ0g2MCyjZol0GiWk9643x9wGR8dwTwfeIJWSefvgdpAR9U8P9jJbCM0VBO-gC4rtstWB3UFUMsDV8AE6okedSaoVNu0BNO8NpxfflqNB6oh3oJW4kHvuJnHPCPly0mZWrY-QU_QhUcLgWd12SrQaCgYKAVoSARISFQHGX2MiLTsc60kevgi5VbtqxCGF_Q0177");
+            GoogleMeetDto googleMeetingLink= googleMeetService.googlemeetrequest(googleMeetRequestDto);
+            log.info(googleMeetingLink.toString());
+
+             */
 
             // 계좌이체 후 DTO 반환
             BankTransferDto bankTransferDto = new BankTransferDto();
             bankTransferDto.setGoogleMeetingLink("구글미팅 링크 생성로직은 아직 구현 전..");
+            //String googlemeetlink=googleMeetingLink.getGoogleMeetingLink();
+            //bankTransferDto.setGoogleMeetingLink(googlemeetlink);
             bankTransferDto.setCreated_at(new Date());
             return bankTransferDto;
         } catch (Exception ex) {
