2e69b36032a41a927de7b7d0b88e8e2d3b5bbb9c
혜원 <bogum1029@gmail.com>
2025-02-17 15:45:24 +0900

    feat: 디자이너 기본 엔티티 생성 (#8)

---
+++ src/main/java/com/haertz/be/designer/entity/Designer.java
@@ -0,0 +1,22 @@
+package com.haertz.be.designer.entity;
+
+import com.haertz.be.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "designer")
+public class Designer extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "designer_id")
+    private Long designer_id;
+
+    @Column(name = "designer_name")
+    private String designerName;
+}
---
+++ src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
@@ -0,0 +1,9 @@
+package com.haertz.be.designer.repository;
+
+import com.haertz.be.designer.entity.Designer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DesignerRepository extends JpaRepository<Designer, Long> {
+}
