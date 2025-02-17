52e00bf48e7004cc4dae2d89d4b9d68b398dd392
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-17 19:34:24 +0900

    feat : 예약 리스트 가져오는 메서드 추가 (#6)

--- src/main/java/com/haertz/be/auth/entity/User.java
+++ src/main/java/com/haertz/be/auth/entity/User.java
@@ -1,9 +1,13 @@
 package com.haertz.be.auth.entity;
 
+import com.haertz.be.booking.entity.Booking;
 import com.haertz.be.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -24,4 +28,6 @@ public class User extends BaseTimeEntity {
     @NotNull
     private AuthInfo authInfo;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Booking> bookings = new ArrayList<>();
 }
