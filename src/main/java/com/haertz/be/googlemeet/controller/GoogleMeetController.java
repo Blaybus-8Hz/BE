30d1620868de95d0eb14afb80c2a185e867eca44
byunhm02 <128574532+byunhm02@users.noreply.github.com>
2025-02-17 23:36:18 +0900

    docs: 스웨거에 API설명 수정(#13)

--- src/main/java/com/haertz/be/googlemeet/controller/GoogleMeetController.java
+++ src/main/java/com/haertz/be/googlemeet/controller/GoogleMeetController.java
@@ -16,14 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "구글 미팅 링크 생성 API", description = "구글미팅 링크생성을 위한 테스트용 API입니다.")
+@Tag(name = "구글 미팅 링크 생성 API", description = "구글미팅 링크생성 테스트를 위한 백엔드 테스트용 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @Log
 public class GoogleMeetController {
     private final GoogleMeetService googleMeetService;
 
-    @Operation(summary = "해당 api를 통해 구글미팅 링크가 잘 생성되는지 테스트 할 수 있습니다.")
+    @Operation(summary = "구글 캘린더 API를 사용하여 구글미팅 링크가 잘 생성되었는지 테스트 할 수 있습니다.")
     @PostMapping("/api/test/googlemeet")
     public ResponseEntity<GoogleMeetDto> GoogleMeet(@RequestBody GoogleMeetRequestDto requestDTO) {
         GoogleMeetDto googleMeetDto = googleMeetService.googlemeetrequest(requestDTO);
