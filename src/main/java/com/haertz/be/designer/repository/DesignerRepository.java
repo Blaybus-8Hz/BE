b8b0cfd9dea467cf9172e10c3c170e2b257a9f97
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-17 21:33:54 +0900

    feat : 라이브러리 추가 (#18)

--- src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
+++ src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
@@ -4,6 +4,10 @@ import com.haertz.be.designer.entity.Designer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DesignerRepository extends JpaRepository<Designer, Long> {
+
+    Designer findByDesignerId(Long designerId);
 }
--- src/main/java/com/haertz/be/designer/service/DesignerService.java
+++ src/main/java/com/haertz/be/designer/service/DesignerService.java
@@ -1,13 +1,21 @@
 package com.haertz.be.designer.service;
 
+import com.haertz.be.auth.exception.UserErrorCode;
+import com.haertz.be.common.exception.base.BaseException;
+import com.haertz.be.designer.dto.DesignerResponse;
+import com.haertz.be.designer.entity.Designer;
 import com.haertz.be.designer.repository.DesignerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class DesignerService {
-    private final DesignerRepository designerRepository;
 
+
+    private final ModelMapper modelMapper;
+
+    private final DesignerRepository designerRepository;
 
 }
