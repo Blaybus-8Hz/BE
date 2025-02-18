31f051b3594c00fca89dd1b5f2e059100fbfb805
혜원 <bogum1029@gmail.com>
2025-02-18 23:28:49 +0900

    feat: 예약 취소 API 구현 (#29)

--- src/main/java/com/haertz/be/booking/controller/BookingController.java
+++ src/main/java/com/haertz/be/booking/controller/BookingController.java
@@ -6,6 +6,7 @@ import com.haertz.be.booking.entity.Booking;
 import com.haertz.be.booking.service.BookingDomainService;
 import com.haertz.be.booking.service.BookingService;
 import com.haertz.be.booking.usecase.BookUseCase;
+import com.haertz.be.booking.usecase.CancelBookingWithRefundUseCase;
 import com.haertz.be.booking.usecase.GetAvailableDatesUseCase;
 import com.haertz.be.booking.usecase.GetAvailableTimesUseCase;
 import com.haertz.be.common.response.SuccessResponse;
@@ -30,6 +31,7 @@ public class BookingController {
     private final BookUseCase bookUseCase;
     private final GetAvailableDatesUseCase getAvailableDatesUseCase;
     private final GetAvailableTimesUseCase getAvailableTimesUseCase;
+    private final CancelBookingWithRefundUseCase cancelBookingWithRefundUseCase;
 
     private final BookingService bookingService;
 
@@ -54,11 +56,17 @@ public class BookingController {
         return SuccessResponse.of(Collections.singletonMap("예약 가능 시간 리스트: ", availableTimes));
     }
 
+    @Operation(summary = "선택한 예약 일정을 취소합니다.")
+    @DeleteMapping("/{bookingId}")
+    public SuccessResponse<Void> cancelBookingWithRefund(@PathVariable Long bookingId){
+        cancelBookingWithRefundUseCase.execute(bookingId);
+        return SuccessResponse.empty();
+    }
+
     @Operation(summary = "사용자의 다가오는 예약 내역을 조회합니다.")
     @GetMapping("/current")
     public SuccessResponse<Object> getBookings(){
         List<Booking> bookingList = bookingService.getCurrentBookings();
-
         return SuccessResponse.of(bookingList);
     }
 
@@ -66,7 +74,6 @@ public class BookingController {
     @GetMapping("/past")
     public SuccessResponse<Object> getPastBookings(){
         List<Booking> bookingList = bookingService.getPastBookings();
-
         return SuccessResponse.of(bookingList);
     }
 }
