dda3088b5514b77a97475a78f9dd051681958ef6
혜원 <bogum1029@gmail.com>
2025-02-18 20:58:46 +0900

    feat: 컨설팅 디자이너 예약 가능 날짜 및 시간 api 구현 (#38)

--- src/main/java/com/haertz/be/booking/controller/BookingController.java
+++ src/main/java/com/haertz/be/booking/controller/BookingController.java
@@ -2,17 +2,22 @@ package com.haertz.be.booking.controller;
 
 import com.haertz.be.booking.dto.request.BookingInfoRequest;
 import com.haertz.be.booking.dto.response.BookingResponse;
-import com.haertz.be.booking.service.BookingDomainService;
 import com.haertz.be.booking.usecase.BookUseCase;
+import com.haertz.be.booking.usecase.GetAvailableDatesUseCase;
+import com.haertz.be.booking.usecase.GetAvailableTimesUseCase;
 import com.haertz.be.common.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,11 +25,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/booking")
 public class BookingController {
     private final BookUseCase bookUseCase;
+    private final GetAvailableDatesUseCase getAvailableDatesUseCase;
+    private final GetAvailableTimesUseCase getAvailableTimesUseCase;
 
     @Operation(summary = "헤어 컨설팅 일정 예약을 요청합니다.")
     @PostMapping
-    public SuccessResponse<BookingResponse> book(@RequestBody @Valid BookingInfoRequest bookingInfoRequest){
+    public SuccessResponse<BookingResponse> book(@RequestBody @Valid BookingInfoRequest bookingInfoRequest) {
         BookingResponse bookingResponse = bookUseCase.execute(bookingInfoRequest);
         return SuccessResponse.of(bookingResponse);
+    }
+
+    @Operation(summary = "해당 디자이너의 3개월 내 예약 가능 날짜를 조회합니다.")
+    @GetMapping("/available-dates")
+    public SuccessResponse<Map<String, List<LocalDate>>> getAvailableDates(@RequestParam @Min(1) Long designerId) {
+        List<LocalDate> availableDates = getAvailableDatesUseCase.execute(designerId);
+        return SuccessResponse.of(Collections.singletonMap("예약 가능 날짜 리스트: ", availableDates));
+    }
+
+    @Operation(summary = "해당 디자이너의 선택한 날짜의 예약 가능 시간을 조회합니다.")
+    @GetMapping("/available-times")
+    public SuccessResponse<Map<String, List<LocalTime>>> getAvailableTimes(@RequestParam @Min(1) Long designerId, @RequestParam LocalDate bookingDate) {
+        List<LocalTime> availableTimes = getAvailableTimesUseCase.execute(designerId, bookingDate);
+        return SuccessResponse.of(Collections.singletonMap("예약 가능 시간 리스트: ", availableTimes));
     }
 }
