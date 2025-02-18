d3444926ff355134d41564b09462c82b01928d2b
혜원 <bogum1029@gmail.com>
2025-02-18 12:39:59 +0900

    refactor: @Slf4j 어노테이션 제거 (#8)

--- src/main/java/com/haertz/be/user/usecase/GetUserProfileUseCase.java
+++ src/main/java/com/haertz/be/user/usecase/GetUserProfileUseCase.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @UseCase
 @RequiredArgsConstructor
-@Slf4j
 public class GetUserProfileUseCase {
     private final AuthenticatedUserUtils userUtils;
     public UserProfileDto execute(){
