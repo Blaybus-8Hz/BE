e6628202807aded938e77e55b81fca95a4f77913
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-18 17:39:30 +0900

    feat : 예약 조회 서비스 추가 (#35)

--- src/main/java/com/haertz/be/booking/service/BookingService.java
+++ src/main/java/com/haertz/be/booking/service/BookingService.java
@@ -1,9 +1,42 @@
 package com.haertz.be.booking.service;
 
+import com.haertz.be.auth.adaptor.UserAdaptor;
+import com.haertz.be.auth.entity.User;
+import com.haertz.be.booking.entity.Booking;
+import com.haertz.be.booking.repository.BookingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BookingService {
+    private final BookingRepository bookingRepository;
+    private final UserAdaptor userAdaptor;
+
+    public List<Booking> getPastBookings(Long userId){
+        User user = userAdaptor.findById(userId);
+        LocalDateTime now = LocalDateTime.now();
+        return user.getBookings().stream()
+                .filter(booking -> {
+                    LocalDateTime bookingDateTime = LocalDateTime.of(booking.getBookingDate(), booking.getBookingTime());
+                    return bookingDateTime.isBefore(now);  // 과거 예약
+                })
+                .collect(Collectors.toList());
+    }
+
+    public List<Booking> getCurrentBookings(Long userId){
+        User user = userAdaptor.findById(userId);
+        LocalDateTime now = LocalDateTime.now();
+        return user.getBookings().stream()
+                .filter(booking -> {
+                    LocalDateTime bookingDateTime = LocalDateTime.of(booking.getBookingDate(), booking.getBookingTime());
+                    return bookingDateTime.isAfter(now);  // 미래 예약
+                })
+                .collect(Collectors.toList());
+    }
+
 }
