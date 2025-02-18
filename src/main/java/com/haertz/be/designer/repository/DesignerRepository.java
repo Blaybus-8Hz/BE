5ccd1a594294e7cedc58b2f1fcbb0f56c4665130
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-19 02:06:47 +0900

    feat : 디자이너 카테고리 별 api 구현(#35)

--- src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
+++ src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
@@ -1,11 +1,20 @@
 package com.haertz.be.designer.repository;
 
 import com.haertz.be.designer.entity.Designer;
+import com.haertz.be.designer.entity.Specialty;
+import io.micrometer.common.lang.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DesignerRepository extends JpaRepository<Designer, Long> {
 
     Designer findByDesignerId(Long designerId);
+    List<Designer> findByDesignerSpecialty(Specialty specialty);
+
+    @Override
+    @NonNull
+    List<Designer> findAll();
 }
