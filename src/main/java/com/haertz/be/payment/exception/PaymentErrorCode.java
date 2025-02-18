3f0cc46ecce4fd0f6d67a10002971943a16b5c59
혜원 <bogum1029@gmail.com>
2025-02-18 12:56:02 +0900

    feat: 결제 상태 유효하지 않았을 때 에러코드 추가 (#8)

--- src/main/java/com/haertz/be/payment/exception/PaymentErrorCode.java
+++ src/main/java/com/haertz/be/payment/exception/PaymentErrorCode.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum PaymentErrorCode implements BaseErrorCode {
     INVALID_PAYMENT_REQUEST(400, "PAYMENT_400", "잘못된 결제 요청입니다."),
     PAYMENT_NOT_FOUND(404, "PAYMENT_404", "결제 내역을 찾을 수 없습니다."),
+    PAYMENT_STATUS_INVALID(400, "PAYMENT_STATUS_", "결제 상태가 유효하지 않습니다."),
     PAYMENT_PROCESSING_ERROR(500, "PAYMENT_500", "결제 처리 중 오류가 발생했습니다.");
 
     private final int httpStatus;
