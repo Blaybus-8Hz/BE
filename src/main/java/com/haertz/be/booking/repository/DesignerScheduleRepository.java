eec88c28924b3f910f11e73d6ae7848b875d8cd9
혜원 <bogum1029@gmail.com>
2025-02-18 20:58:16 +0900

    feat: 헤어 컨설팅 디자이너의 예약 가능 시간 및 날짜 반환 로직 구현 (#38)

--- src/main/java/com/haertz/be/booking/repository/DesignerScheduleRepository.java
+++ src/main/java/com/haertz/be/booking/repository/DesignerScheduleRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public interface DesignerScheduleRepository extends JpaRepository<DesignerSchedule, Long> {
     @Query("SELECT COUNT(ds) > 0 FROM DesignerSchedule ds " +
@@ -23,5 +24,20 @@ public interface DesignerScheduleRepository extends JpaRepository<DesignerSchedu
                                  @Param("bookingDate") LocalDate bookingDate,
                                  @Param("bookingTime") LocalTime bookingTime);
 
+    @Query("SELECT ds.bookingDate FROM DesignerSchedule ds " +
+            "WHERE ds.designerId = :designerId " +
+            "AND ds.bookingDate BETWEEN :startDate AND :endDate " +
+            "GROUP BY ds.bookingDate " +
+            "HAVING COUNT(ds) >= :maxSlots")
+    List<LocalDate> findFullyBookedDates(@Param("designerId") Long designerId,
+                                         @Param("startDate") LocalDate startDate,
+                                         @Param("endDate") LocalDate endDate,
+                                         @Param("maxSlots") long maxSlots);
 
+    // 해당 날짜의 예약된 시간 조회
+    @Query("SELECT ds.bookingTime FROM DesignerSchedule ds " +
+            "WHERE ds.designerId = :designerId " +
+            "AND ds.bookingDate = :bookingDate")
+    List<LocalTime> findBookedTimes(@Param("designerId") Long designerId,
+                                    @Param("bookingDate") LocalDate bookingDate);
 }
