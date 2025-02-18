bbeba93f7c97252f52e947fb201d5bef0cf71f83
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-02-18 17:33:49 +0900

    feat : 예약 조회 기능 api 추가 (#36)

--- src/main/java/com/haertz/be/booking/controller/BookingController.java
+++ src/main/java/com/haertz/be/booking/controller/BookingController.java
@@ -2,6 +2,7 @@ package com.haertz.be.booking.controller;
 
 import com.haertz.be.booking.dto.request.BookingInfoRequest;
 import com.haertz.be.booking.dto.response.BookingResponse;
+import com.haertz.be.booking.entity.Booking;
 import com.haertz.be.booking.service.BookingDomainService;
 import com.haertz.be.booking.usecase.BookUseCase;
 import com.haertz.be.common.response.SuccessResponse;
@@ -9,10 +10,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,11 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/booking")
 public class BookingController {
     private final BookUseCase bookUseCase;
+    private final BookingService bookingService;
 
     @Operation(summary = "헤어 컨설팅 일정 예약을 요청합니다.")
     @PostMapping
     public SuccessResponse<BookingResponse> book(@RequestBody @Valid BookingInfoRequest bookingInfoRequest){
         BookingResponse bookingResponse = bookUseCase.execute(bookingInfoRequest);
         return SuccessResponse.of(bookingResponse);
+    }
+
+    @Operation(summary = "사용자의 다가오는 예약 내역을 조회합니다.")
+    @GetMapping("/api/current/{userId}")
+    public SuccessResponse<Object> getBookings(@PathVariable("userId") Long userId){
+        List<Booking> bookingList = bookingService.getCurrentBookings(userId);
+
+        return SuccessResponse.of(bookingList);
+    }
+
+    @Operation(summary = "사용자의 지난 예약 내역을 조회합니다.")
+    @GetMapping("/api/past/{userId}")
+    public SuccessResponse<Object> getPastBookings(@PathVariable("userId") Long userId){
+        List<Booking> bookingList = bookingService.getPastBookings(userId);
+
+        return SuccessResponse.of(bookingList);
     }
 }
---
+++ src/main/java/com/haertz/be/booking/service/BookingService.java
@@ -0,0 +1,9 @@
+package com.haertz.be.booking.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookingService {
+}
