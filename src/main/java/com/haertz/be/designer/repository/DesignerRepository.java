8b99ef8dd1713a36871b94b448289d243723846a
혜원 <bogum1029@gmail.com>
2025-02-20 12:42:29 +0900

    fix: 예약내역 조회 문제 해결(#66)

--- src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
+++ src/main/java/com/haertz/be/designer/repository/DesignerRepository.java
@@ -5,7 +5,12 @@ import com.haertz.be.designer.entity.District;
 import com.haertz.be.designer.entity.MeetingMode;
 import com.haertz.be.designer.entity.Specialty;
 import io.micrometer.common.lang.NonNull;
+import io.micrometer.common.lang.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,13 +18,21 @@ import java.util.List;
 @Repository
 public interface DesignerRepository extends JpaRepository<Designer, Long> {
 
+    Page<Designer> findAll(@NonNull Pageable pageable);
+
+    @Query("SELECT d FROM Designer d WHERE " +
+            "(:meetingMode IS NULL OR d.meetingMode = :meetingMode OR d.meetingMode = 'BOTH') AND " +
+            "(:district IS NULL OR d.designerDistrict = :district) AND " +
+            "(:specialty IS NULL OR d.designerSpecialty = :specialty)")
+    Page<Designer> findFilteredDesigners(
+            @Nullable @Param("meetingMode") MeetingMode meetingMode,
+            @Nullable @Param("district") District district,
+            @Nullable @Param("specialty") Specialty specialty,
+            Pageable pageable
+    );
+
     Designer findByDesignerId(Long designerId);
     List<Designer> findByDesignerSpecialty(Specialty specialty);
-
-    @Override
-    @NonNull
-    List<Designer> findAll();
-
     List<Designer> findByDesignerDistrict(District district);
     List<Designer> findByMeetingMode(MeetingMode meetingMode);
  }
